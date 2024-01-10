f366d2475d167f5ba24fae765dd9edc3e13eef8f
tori-calkins <tori.calkins2705@gmail.com>
2024-01-10 14:03:49 -0800

    made footer icons and linked them to their links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home/>}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import profilepic from '../assets/profilepicupdated.jpg'
-import icon from'../assets/linkedin.png'
-
 
 const Home = () => {
 
@@ -11,7 +9,6 @@ const Home = () => {
             <h1 id="greeting">Hi, I'm Tori!</h1>
             <p id="hometext">I'm a junior full-stack developer based in San Diego, California. I've worked on web development projects using HTML, CSS, JavaScript, React.js, Ruby, and Ruby on Rails, and my passion for creative problem-solving drives my constant skill growth.</p>
             <img id="profilepicture" src={profilepic} alt="Profile Picture of me"/>
-            <img src="../assets/linkedin.png"/>
         </div>
         </>
     )
