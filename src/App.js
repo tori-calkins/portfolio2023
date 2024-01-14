88d26a84aa1a8da65b256aaaa6711473c76d664d
tori-calkins <tori.calkins2705@gmail.com>
2024-01-13 17:39:27 -0800

    got the contact me form set up and sends me emails

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/Home'
 import AboutMe from './pages/AboutMe'
 import Projects from './pages/Projects'
+import ContactMe from './pages/ContactMe'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Routes, Route} from 'react-router-dom'
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/" element={<Home/>}/>
       <Route path="/aboutme" element={<AboutMe/>}/>
       <Route path="/projects" element={<Projects/>}/>
+      <Route path="/contactme" element={<ContactMe/>}/>
     </Routes>
     <Footer/>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
             <NavLink className="navlinks" to="/">Home</NavLink>
             <NavLink className="navlinks" to="/aboutme" >About Me</NavLink>
             <NavLink className="navlinks" to="/projects">Projects</NavLink>
-            <NavLink className="navlinks">Contact Me</NavLink>
+            <NavLink className="navlinks" to="/contactme">Contact Me</NavLink>
         </div>
         </>
     )
